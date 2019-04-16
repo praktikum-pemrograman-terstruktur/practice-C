736472650765b7f7694cad41a20e817b749b099d
unknown <geoivans@yahoo.com>
2019-04-16 20:49:33 +0700

    Grego Ivan S.E. - Pertemuan 1

---
+++ Grego Ivan S.E. - Pertemuan 1/helloworld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("Welcome to C programming.", &s);
+    printf("Hello, World!\n");
+    printf("Welcome to C programming.");
+
+    return 0;
+}
