756e106c7daae4ae70292b67e440f63e68ab2998
Ervan Andi <ervan.andi45@gmail.com>
2019-04-17 23:01:40 +0700

    Aldi Dwi Putra - Pertemuan 1

---
+++ Aldi Dwi Putra - Pertemuan 1/hello world.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int main(){
+ char ch,s[10],sen[20],sun[20],son[20];
+ scanf("%c",&ch);
+ scanf("%s",&s);
+ scanf("%s",&sen);
+ scanf("%s",&sun);
+ scanf("%s",&sin);
+ printf("%c \n",ch);
+ printf("%s",s);
+ printf("%s \n",sen);
+ printf("%s",sun);
+ printf("%s",son);
+ return 0;
+}
